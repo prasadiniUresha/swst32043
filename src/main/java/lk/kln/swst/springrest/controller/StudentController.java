83d60af4f6818ad19f142e3e409f9922ef1959e4
prasadiniUresha <prasadinipadmasiri@gmail.com>
2019-12-07 12:39:23 +0530

    implementing jpa

--- src/main/java/lk/kln/swst/springrest/controller/StudentController.java
+++ src/main/java/lk/kln/swst/springrest/controller/StudentController.java
@@ -1,6 +1,8 @@
 package lk.kln.swst.springrest.controller;
 
 import lk.kln.swst.springrest.model.Student;
+import lk.kln.swst.springrest.repository.StudentRepository;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/students")
 public class StudentController {
 
+    StudentRepository studentRepository;
+
     @RequestMapping(method = RequestMethod.GET)
     public Student get() throws IllegalArgumentException{
         Student student = new Student();
         student.setName("Prasadini");
         student.setAge(23);
         return student;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public  Student save(@RequestBody Student student){
+        return studentRepository.save(student);
     }
 }
--- src/main/java/lk/kln/swst/springrest/model/Student.java
+++ src/main/java/lk/kln/swst/springrest/model/Student.java
@@ -1,9 +1,18 @@
 package lk.kln.swst.springrest.model;
 
 public class Student {
+    private Long id;
     private String name;
     private String address;
     private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
